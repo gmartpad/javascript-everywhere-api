0f56dc851a3a36ea40866e45c5f34d75b57a5a9d
Gabriel MP <contato.gabrielmartinspadoin@gmail.com>
2023-02-23 05:56:19 -0300

    user-actions - feat: adicionando a acao de usuario onde pode se favoritar uma anotacao

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,10 +1,14 @@
 const Query = require('./query');
 const Mutation = require('./mutation');
+const Note = require('./note');
+const User = require('./user');
 const { GraphQLDateTime } = require('graphql-iso-date');
 
 // Provide resolver functions for our schema fields
 module.exports = {
   Query,
   Mutation,
+  Note,
+  User,
   DateTime: GraphQLDateTime
 };
